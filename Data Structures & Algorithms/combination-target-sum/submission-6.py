875d02cf804d431c7e3db513f28ced8eac7f49bd
AryanG2020 <70577796+AryanG2020@users.noreply.github.com>
2026-05-27 19:59:11 -0400

    Add: combination-target-sum - submission-6

---
+++ Data Structures & Algorithms/combination-target-sum/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res, sol=[],[]
+        #myset=set()
+        def dfs(n, total):
+            if total>target or n==len(nums):
+                return 
+            if total==target: #and tuple(sol) not in myset:
+                res.append(sol.copy())
+                return #prevents duplication
+                #myset.add(tuple(sol))
+            if total+nums[n]>target:
+                return
+            sol.append(nums[n])
+            dfs(n, total+nums[n])
+            sol.pop()
+            dfs(n+1, total)
+        dfs(0,0)
+        return res
+            
+
+        
